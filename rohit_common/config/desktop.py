db8c206cbfe90af23b46d2ebd7d26a76222be172
Anand Doshi <anand@erpnext.com>
2014-06-12 19:01:37 +0530

    First commit

---
+++ rohit_common/config/desktop.py
@@ -0,0 +1,11 @@
+from frappe import _
+
+def get_data():
+	return {
+		"Rohit ERPNext Extensions (Common)": {
+			"color": "#007AFF",
+			"icon": "icon-paper-clip",
+			"type": "module",
+			"label": _("Rohit ERPNext Extensions (Common)")
+		}
+	}
---
+++ rohit_common/hooks.py
@@ -0,0 +1,94 @@
+app_name = "rohit_common"
+app_title = "Rohit ERPNext Extensions (Common)"
+app_publisher = "Rohit Industries Ltd."
+app_description = "Rohit ERPNext Extensions (Common)"
+app_icon = "icon-paper-clip"
+app_color = "#007AFF"
+app_email = "aditya@rigpl.com"
+app_url = "https://github.com/adityaduggal/rohit_common"
+app_version = "0.0.1"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/rohit_common/css/rohit_common.css"
+# app_include_js = "/assets/rohit_common/js/rohit_common.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/rohit_common/css/rohit_common.css"
+# web_include_js = "/assets/rohit_common/js/rohit_common.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Installation
+# ------------
+
+# before_install = "rohit_common.install.before_install"
+# after_install = "rohit_common.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "rohit_common.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.core.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.core.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"rohit_common.tasks.all"
+# 	],
+# 	"daily": [
+# 		"rohit_common.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"rohit_common.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"rohit_common.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"rohit_common.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "rohit_common.install.before_tests"
+
