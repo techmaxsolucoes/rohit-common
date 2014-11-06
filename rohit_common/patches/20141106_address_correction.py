1798c7d7b9e3051a79f3f6ea6575389d5808074c
Aditya Duggal <adityaduggal@gmail.com>
2014-11-06 22:19:39 +0530

    Final Commit for Transferring Tax Numbers to Address Table

--- rohit_common/patches/20141106_address_correction.py
+++ rohit_common/patches/20141106_address_correction.py
@@ -4,8 +4,29 @@ import frappe
 from frappe import msgprint
 
 def execute():
-	#correct the address type
-	for name in frappe.db.sql_list("""select name from tabAddress 
-		where address_type NOT IN ("Billing" , "Shipping", "Office", "Personal", "Plant", "Postal", "Shop", "Subsidiary", "Warehouse", "Other")""")
+	#correct the address table
+	for name in frappe.db.sql_list("""select name from tabAddress"""):
 		add = frappe.get_doc("Address", name)
-		frappe.db.set_value("Address", add.name, address_type, "Billing")
+		if add.address_type not in ("Billing" , "Shipping", "Office", "Personal", "Plant", "Postal", "Shop", "Subsidiary", "Warehouse", "Other"):
+			frappe.db.set_value("Address", add.name, "address_type", "Billing")
+		if not add.address_line1:
+			frappe.db.set_value("Address", add.name, "address_line1", "NA")
+		if not add.city:
+			frappe.db.set_value("Address", add.name, "city", "NA")
+		if not add.country:
+			frappe.db.set_value("Address", add.name, "country", "India")
+		if not add.phone:
+			frappe.db.set_value("Address", add.name, "phone", "NA")
+		if add.customer:
+			tin = frappe.db.get_value ("Customer", add.customer, "tin_no")
+			ecc = frappe.db.get_value ("Customer", add.customer, "excise_no")
+			frappe.db.set_value("Address", add.name, "tin_no", tin)
+			frappe.db.set_value("Address", add.name, "excise_no", ecc)
+			frappe.db.set_value("Address", add.name, "service_tax_no", "NA")
+		if add.supplier:
+			tin = frappe.db.get_value ("Supplier", add.supplier, "tin_no")
+			ecc = frappe.db.get_value ("Supplier", add.supplier, "excise_no")
+			stax = frappe.db.get_value("Supplier",add.supplier, "service_tax_no")
+			frappe.db.set_value("Address", add.name, "tin_no", tin)
+			frappe.db.set_value("Address", add.name, "excise_no", ecc)
+			frappe.db.set_value("Address", add.name, "service_tax_no", stax)
