482a008a78d772ea855d911bf542cb752d0b5529
Aditya Duggal <adityaduggal@gmail.com>
2015-01-09 13:18:05 +0530

    Added purchase tax report

---
+++ rohit_common/rohit_common/report/st_return_purchase/st_return_purchase.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2013, Rohit Industries Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["ST Return Purchase"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": "From Date",
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": "To Date",
+			"fieldtype": "Date",
+			"default": get_today()
+		},
+		{
+			"fieldname":"supplier",
+			"label": __("Supplier"),
+			"fieldtype": "Link",
+			"options": "Supplier"
+		},
+		{
+			"fieldname":"letter_head",
+			"label": "Letter Head",
+			"fieldtype": "Link",
+			"options": "Letter Head"
+		}
+	]
+}
---
+++ rohit_common/rohit_common/report/st_return_purchase/st_return_purchase.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2013, Web Notes Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import flt
+from frappe import msgprint, _
+
+def execute(filters=None):
+	if not filters: filters = {}
+
+	columns = get_columns()
+	data = get_invoices(filters)
+	return columns, data
+	
+def get_columns():
+	return [
+		"Supplier:Link/Supplier:200", "Purchase Type::180", "TIN# on Master::100",
+		"Net Total:Currency:100", "Net Total Before Sales Tax:Currency:100", "Sales Tax %age:Percent:50",
+		"Total Sales Tax Amount:Currency:100", "Grand Total:Currency:100", "ST % Calculated:Float:50"
+	]
+
+def get_invoices(filters):
+	conditions = get_conditions(filters)
+	
+	query = """SELECT pi.supplier, pi.taxes_and_charges, pi.supplier_address, sum(pi.net_total),
+	sum(pi_tax.total), pi_tax.rate, sum(pi_tax.tax_amount), 
+	sum(pi.grand_total), null
+	FROM `tabPurchase Invoice` pi, `tabPurchase Taxes and Charges` pi_tax
+	WHERE pi.docstatus = 1 AND pi_tax.parent = pi.name 
+	AND (select tax_type from `tabAccount` where 
+	name = pi_tax.account_head) REGEXP 'Sales Tax' %s
+	GROUP BY pi.supplier, pi.taxes_and_charges
+	ORDER BY pi.supplier""" % conditions
+	
+	#frappe.msgprint (query)
+	
+	si= frappe.db.sql(query, as_list=1)
+	tin = frappe.db.sql ("""SELECT name, supplier, tin_no from `tabAddress` """, as_list=1)
+	
+	#frappe.msgprint(len(si))
+	
+	for i in range(0, len(si)):	
+		si[i][4] = si[i][4]-si[i][6]
+		si[i][8] = round((si[i][6]*100)/si[i][4],2)
+		for j in range(0,len(tin)):
+			if si[i][2]==tin[j][0]:
+				si[i][2]= tin[j][2]
+
+	return si
+	
+
+def get_conditions(filters):
+	conditions = ""
+
+	if filters.get("from_date"):
+		conditions += " and pi.posting_date >= '%s'" % filters["from_date"]
+
+	if filters.get("to_date"):
+		conditions += " and pi.posting_date <= '%s'" % filters["to_date"]
+	
+	if filters.get("supplier"):
+		conditions += " and pi.supplier = '%s'" % filters["supplier"]
+
+	if filters.get("letter_head"):
+		conditions += " and pi.letter_head = '%s'" % filters["letter_head"]
+
+
+	return conditions
