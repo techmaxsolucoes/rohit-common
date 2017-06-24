5d8e313677112b05d6d9abc11503a4ee98477463
Aditya Duggal <adityaduggal@gmail.com>
2017-06-24 18:13:59 +0530

    Added the following GST features -Custom fields for Sales Invoice to JSON -Custom fields for Address moved to JSON -Sales Taxes template to JSON -Sales Invoice validation for GST tax type would now check for STATE and for EXPORT as well

--- rohit_common/rohit_common/validations/sales_invoice.py
+++ rohit_common/rohit_common/validations/sales_invoice.py
@@ -4,16 +4,35 @@ import frappe
 from frappe import msgprint
 	
 def validate(doc,method):
+
 	bill_tin =frappe.db.get_value("Address", doc.customer_address ,"tin_no")
 	bill_excise =frappe.db.get_value("Address", doc.customer_address ,"excise_no")
 	ship_tin =frappe.db.get_value("Address", doc.shipping_address_name ,"tin_no")
 	ship_excise =frappe.db.get_value("Address", doc.shipping_address_name ,"excise_no")
+	ship_gstin = frappe.db.get_value("Address", doc.shipping_address_name ,"gstin")
+	bill_gstin = frappe.db.get_value("Address", doc.customer_address ,"gstin")
+	ship_state = frappe.db.get_value("Address", doc.shipping_address_name, "state_rigpl")
+	ship_country = frappe.db.get_value("Address", doc.shipping_address_name, "country")
+	template_doc = frappe.get_doc("Sales Taxes and Charges Template", doc.taxes_and_charges)
+	'''
+	doc.ship_gstin = ship_gstin
+	doc.bill_gstin = bill_gstin
+	'''	
 	
-	if (doc.tin_no != bill_tin):
-		frappe.msgprint("TIN No does no match with Billing Address TIN No, please reload Billing Address", raise_exception=1)
-	if (doc.excise_no != bill_excise):
-		frappe.msgprint("Excise No does no match with Billing Address Excise No, please reload Billing Address", raise_exception=1)
-	if (doc.shipping_tin_no != ship_tin):
-		frappe.msgprint("TIN No does no match with Shipping Address TIN No, please reload Shipping Address", raise_exception=1)
-	if (doc.shipping_excise_no != ship_excise):
-		frappe.msgprint("Excise No does no match with Shipping Address Excise No, please reload Shipping Address", raise_exception=1)
+	#Check if Shipping State is Same as Template State then check if the tax template is LOCAL
+	#Else if the States are different then the template should NOT BE LOCAL
+	if ship_state == template_doc.state:
+		if template_doc.is_local_sales != 1:
+			frappe.throw(("Selected Tax {0} is NOT LOCAL Tax but Shipping Address is \
+				in Same State {1}, hence either change Shipping Address or Change the \
+				Selected Tax").format(doc.taxes_and_charges, ship_state))
+	elif ship_country == 'India' and ship_state != template_doc.state:
+		if template_doc.is_local_sales == 1:
+			frappe.throw(("Selected Tax {0} is LOCAL Tax but Shipping Address is \
+				in Different State {1}, hence either change Shipping Address or Change the \
+				Selected Tax").format(doc.taxes_and_charges, ship_state))
+	elif ship_country != 'India': #Case of EXPORTS
+		if template_doc.state is not None:
+			frappe.throw(("Selected Tax {0} is for Indian Sales but Shipping Address is \
+				in Different Country {1}, hence either change Shipping Address or Change the \
+				Selected Tax").format(doc.taxes_and_charges, ship_country))
