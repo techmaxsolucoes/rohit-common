5f9585d8a3b556891425e1ee4d70afaddbdaf37b
Aditya Duggal <adityaduggal@gmail.com>
2017-07-25 15:42:54 +0530

    Fixes to GST Offline Import Format Report

--- rohit_common/rohit_common/report/gst_offline_import_format_rigpl/gst_offline_import_format_rigpl.py
+++ rohit_common/rohit_common/report/gst_offline_import_format_rigpl/gst_offline_import_format_rigpl.py
@@ -23,17 +23,18 @@ def get_columns(filters):
 def get_data(filters):
 	si_cond = get_conditions(filters)
 	data = frappe.db.sql("""SELECT ad.gstin, si.name, si.posting_date, si.base_grand_total,
-		si.base_net_total, si.customer, si.shipping_address_name, 
-		si.taxes_and_charges
-		FROM `tabSales Invoice` si, `tabAddress` ad
-		WHERE ad.name = si.shipping_address_name AND si.docstatus = 1 %s
+		CONCAT(st.state_code_numeric, '-', ad.state_rigpl), si.base_net_total,si.customer, 
+		si.shipping_address_name, si.taxes_and_charges
+		FROM `tabSales Invoice` si, `tabAddress` ad, `tabState` st
+		WHERE ad.name = si.shipping_address_name 
+			AND si.docstatus = 1 AND st.name = ad.state_rigpl %s
 		ORDER BY si.posting_date, si.name""" %(si_cond), as_list=1)
 
 	for d in data:
-		tax_doc = frappe.get_doc("Sales Taxes and Charges Template", d[7])
-		state_code = frappe.get_value("State", tax_doc.state, "state_code_numeric")
-		d.insert(4, str(state_code) + "-" + str(tax_doc.state))
-		if len(d[0]) == 15:
+		tax_doc = frappe.get_doc("Sales Taxes and Charges Template", d[8])
+		#state_code = frappe.get_value("State", tax_doc.state, "state_code_numeric")
+		#d.insert(4, str(state_code) + "-" + str(tax_doc.state))
+		if d[0] is not None and len(d[0]) == 15:
 			d.insert(6,"B2B(4)")
 		else:
 			is_export = tax_doc.is_export
