b1771a3cc1ec9f429043497446e924ab4cfb24b0
Aditya Duggal <adityaduggal@gmail.com>
2017-08-03 17:57:22 +0530

    Sales Taxes integrity check fixed

--- rohit_common/rohit_common/validations/sales_invoice.py
+++ rohit_common/rohit_common/validations/sales_invoice.py
@@ -48,12 +48,18 @@ def validate(doc,method):
 
 def check_taxes_integrity(doc,method, template):
 	for tax in doc.taxes:
+		check = 0
 		for temp in template.taxes:
-			if tax.idx == temp.idx:
+			if tax.idx == temp.idx and check == 0:
+				check = 1
 				if tax.charge_type != temp.charge_type or tax.row_id != temp.row_id or \
 					tax.account_head != temp.account_head or tax.included_in_print_rate \
 					!= temp.included_in_print_rate or tax.rate != temp.rate:
 						frappe.throw(("Selected Tax {0}'s table does not match with tax table \
 							of Invoice# {1}. Check Row # {2} or reload Taxes").\
 							format(doc.taxes_and_charges, doc.name, tax.idx))
-	
+		if check == 0:
+			frappe.throw(("Selected Tax {0}'s table does not match with tax table \
+				of Invoice# {1}. Check Row # {2} or reload Taxes").\
+				format(doc.taxes_and_charges, doc.name, tax.idx))
+
