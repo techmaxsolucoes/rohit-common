5aa25c9cf592f362ea156a0f6f8b73635dd83661
Aditya Duggal <adityaduggal@gmail.com>
2017-08-22 11:54:22 +0530

    Fixes to Offline GST Format report -Option to show Item Wise Sales and quantity needed in HSN Code -Export bills are now shown separately.

--- rohit_common/rohit_common/report/gst_offline_import_format_rigpl/gst_offline_import_format_rigpl.py
+++ rohit_common/rohit_common/report/gst_offline_import_format_rigpl/gst_offline_import_format_rigpl.py
@@ -11,45 +11,81 @@ def execute(filters=None):
 	return columns, data
 
 def get_columns(filters):
-	return [
-			_("GSTIN Customer") + "::150", _("Invoice Number") + ":Link/Sales Invoice:100",
-			_("Invoice Date") + ":Date:80", _("Grand Total") + ":Currency:80",
-			_("Place of Supply") + "::80", _("Net Total") + ":Currency:80",
-			_("Type of Sale") + "::80", _("Customer") + ":Link/Customer:200",
-			_("Ship Address") + ":Link/Address:200", 
-			_("Tax") + ":Link/Sales Taxes and Charges Template:150"
-		]
+	if filters.get("export") == 1:
+		columns = [
+				_("Invoice Number") + ":Link/Sales Invoice:100",
+				_("Invoice Date") + ":Date:80", _("Grand Total") + ":Currency:80",
+				_("Net Total") + ":Currency:80", _("Customer") + ":Link/Customer:200",
+				_("Ship Address") + ":Link/Address:200", 
+				_("Tax") + ":Link/Sales Taxes and Charges Template:150", 
+				_("Ship Country") + ":Link/Country:150" 
+			]
+	elif filters.get("hsn") == 1:
+		columns = [
+				_("Item Code") + ":Link/Item:100",
+				_("HSN Code") + "::80", _("Quantity") + ":Float:80", _("UoM") + "::80",
+				_("Grand Total") + ":Currency:80", _("Net Total") + ":Currency:80"
+			]
+	else:
+		columns = [
+				_("GSTIN Customer") + "::150", _("Invoice Number") + ":Link/Sales Invoice:100",
+				_("Invoice Date") + ":Date:80", _("Grand Total") + ":Currency:80",
+				_("Place of Supply") + "::80", _("Net Total") + ":Currency:80",
+				_("Type of Sale") + "::80", _("Customer") + ":Link/Customer:200",
+				_("Ship Address") + ":Link/Address:200", 
+				_("Tax") + ":Link/Sales Taxes and Charges Template:150"
+			]
+	return columns
 
 def get_data(filters):
 	si_cond = get_conditions(filters)
-	data = frappe.db.sql("""SELECT ad.gstin, si.name, si.posting_date, si.base_grand_total,
-		CONCAT(st.state_code_numeric, '-', ad.state_rigpl), si.base_net_total,si.customer, 
-		si.shipping_address_name, si.taxes_and_charges
-		FROM `tabSales Invoice` si, `tabAddress` ad, `tabState` st
-		WHERE ad.name = si.shipping_address_name 
-			AND si.docstatus = 1 AND st.name = ad.state_rigpl %s
-		ORDER BY si.posting_date, si.name""" %(si_cond), as_list=1)
+	if filters.get("export") == 1:
+		data = frappe.db.sql("""SELECT si.name, si.posting_date, si.base_grand_total,
+			si.base_net_total,si.customer, si.shipping_address_name, si.taxes_and_charges,
+			ad.country
+			FROM `tabSales Invoice` si, `tabSales Taxes and Charges Template` stct, `tabAddress` ad
+			WHERE stct.name = si.taxes_and_charges AND ad.name = si.shipping_address_name
+				AND si.docstatus = 1 AND stct.is_export = 1 %s
+			ORDER BY si.posting_date, si.name""" %(si_cond), as_list=1)
+	elif filters.get("hsn") == 1:
+		data = frappe.db.sql("""SELECT sid.item_code, it.customs_tariff_number, SUM(sid.qty),
+			it.stock_uom, SUM(sid.base_amount), SUM(sid.base_net_amount)
+			FROM `tabSales Invoice` si, `tabSales Invoice Item` sid, `tabItem` it
+			WHERE si.docstatus = 1 AND sid.parent = si.name 
+				AND sid.item_code = it.name %s
+			GROUP BY sid.item_code
+			ORDER BY sid.item_code""" %(si_cond), as_list = 1)
+	else:
+		data = frappe.db.sql("""SELECT ad.gstin, si.name, si.posting_date, si.base_grand_total,
+			CONCAT(st.state_code_numeric, '-', ad.state_rigpl), si.base_net_total,si.customer, 
+			si.shipping_address_name, si.taxes_and_charges
+			FROM `tabSales Invoice` si, `tabAddress` ad, `tabState` st
+			WHERE ad.name = si.shipping_address_name 
+				AND si.docstatus = 1 AND st.name = ad.state_rigpl %s
+			ORDER BY si.posting_date, si.name""" %(si_cond), as_list=1)
 
-	for d in data:
-		tax_doc = frappe.get_doc("Sales Taxes and Charges Template", d[8])
-		#state_code = frappe.get_value("State", tax_doc.state, "state_code_numeric")
-		#d.insert(4, str(state_code) + "-" + str(tax_doc.state))
-		if d[0] is not None and len(d[0]) == 15:
-			d.insert(6,"B2B(4)")
-		else:
-			is_export = tax_doc.is_export
-			if is_export == 1:
-				d.insert(6,"EXP(6)")
+		for d in data:
+			tax_doc = frappe.get_doc("Sales Taxes and Charges Template", d[8])
+			#state_code = frappe.get_value("State", tax_doc.state, "state_code_numeric")
+			#d.insert(4, str(state_code) + "-" + str(tax_doc.state))
+			if d[0] is not None and len(d[0]) == 15:
+				d.insert(6,"B2B(4)")
 			else:
-				if d[3] > 250000:
-					d.insert(6,"B2CL(5)")
+				is_export = tax_doc.is_export
+				if is_export == 1:
+					d.insert(6,"EXP(6)")
 				else:
-					d.insert(6,"B2CS(7)")
+					if d[3] > 250000:
+						d.insert(6,"B2CL(5)")
+					else:
+						d.insert(6,"B2CS(7)")
 
 	return data
 
 
 def get_conditions(filters):
+	if filters.get("export") == 1 and filters.get("hsn") == 1:
+		frappe.throw("Only one checkbox allowed to be checked at a given time.")
 	si_cond = ""
 	if filters.get("from_date"):
 		si_cond += " AND si.posting_date >= '%s'" %filters["from_date"]
