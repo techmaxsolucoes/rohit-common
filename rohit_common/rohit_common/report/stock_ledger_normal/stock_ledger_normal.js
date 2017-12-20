a3efda23e2815a0519b5b20c2a03bdd83b9447d3
Aditya Duggal <adityaduggal@gmail.com>
2017-12-20 11:07:11 +0530

    Added the GST related Stock Ledger -Minor Fix on Sales Invoice field for Selecting Type of Terms

---
+++ rohit_common/rohit_common/report/stock_ledger_normal/stock_ledger_normal.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Rohit Industries Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Stock Ledger Normal"] = {
+	"filters": [
+
+	]
+}
