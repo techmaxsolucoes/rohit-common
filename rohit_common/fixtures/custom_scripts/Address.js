64a99fba042031d810a4d7b279abf02a60f50d14
frappe <frappe@www.rigpl.com>
2018-09-26 19:23:30 +0530

    Minor Fiz

--- rohit_common/fixtures/custom_scripts/Address.js
+++ rohit_common/fixtures/custom_scripts/Address.js
@@ -1,9 +1,11 @@
 cur_frm.add_fetch('state_rigpl','state_code_numeric','gst_state_number');
 frappe.ui.form.on('Address', "state_rigpl", function(frm, cdt, cdn){
 	var d = locals[cdt][cdn]
-	frappe.model.set_value(cdt, cdn, "gst_state", d.state_rigpl);
-	frappe.model.set_value(cdt, cdn, "gst_state_number", d.gstin.substring(0,2));
-	cur_frm.refresh_fields();
+	if (d.country === 'India'){
+		frappe.model.set_value(cdt, cdn, "gst_state", d.state_rigpl);
+		frappe.model.set_value(cdt, cdn, "gst_state_number", d.gstin.substring(0,2));
+		cur_frm.refresh_fields();
+	}
 });
 frappe.ui.form.on('Address', "gstin", function(frm, cdt, cdn){
 	var d = locals[cdt][cdn]
