6989d37eb3fd16c4f1832a0322f423b25f633e11
Aditya Duggal <adityaduggal@gmail.com>
2018-12-22 06:29:32 +0530

    Removed CETSH Number from SI and added a patch for the same - SO, SI and DN would have a new PL field - Patch to add field in all the Sales Invoice and SO and DN

---
+++ rohit_common/patches/20181222_update_gst_hsn.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import frappe
+
+def execute():
+	sod_no_cetsh = frappe.db.sql("""SELECT name, item_code ,gst_hsn_code, cetsh_number FROM `tabSales Order Item` 
+		WHERE cetsh_number IS NULL AND gst_hsn_code IS NULL 
+		ORDER BY creation""", as_list=1)
+	if sod_no_cetsh:
+		for sod in sod_no_cetsh:
+			sod_doc = frappe.get_doc("Sales Order Item", sod[0])
+			cetsh_number = frappe.get_value("Item", sod[1], "customs_tariff_number")
+			if cetsh_number:
+				frappe.db.set_value("Sales Order Item", sod[0], "cetsh_number", cetsh_number)
+				frappe.db.set_value("Sales Order Item", sod[0], "gst_hsn_code", cetsh_number)
+				frappe.db.commit()
+				print("Updated CETSH Number and GST HSN Code in Sales Order # " \
+					+ sod_doc.parent + " Item No: " + str(sod_doc.idx))
+			else:
+				print("SO# " + sod_doc.parent + " Item Code: " + sod[1] + \
+					" At Row No " + str(sod_doc.idx) + \
+					" Does Not Have CETSH Number Linked")
+
+	sod_list = frappe.db.sql("""SELECT name, gst_hsn_code, cetsh_number FROM `tabSales Order Item` 
+		WHERE cetsh_number IS NOT NULL AND gst_hsn_code IS NULL 
+		ORDER BY creation""", as_list=1)
+	if sod_list:
+		for sod in sod_list:
+			cetsh_number = frappe.get_value("Sales Order Item", sod[0], "cetsh_number")
+			sod_doc = frappe.get_doc("Sales Order Item", sod[0])
+			frappe.db.set_value("Sales Order Item", sod[0], "gst_hsn_code", cetsh_number)
+			frappe.db.commit()
+			print("Updated GST HSN Code in Sales Order # " + sod_doc.parent + " Item No: " + str(sod_doc.idx))
+
+	dnd_no_cetsh = frappe.db.sql("""SELECT name, item_code, gst_hsn_code, cetsh_number FROM `tabDelivery Note Item` 
+		WHERE cetsh_number IS NULL AND gst_hsn_code IS NULL 
+		ORDER BY creation""", as_list=1)
+	if dnd_no_cetsh:
+		for dnd in dnd_no_cetsh:
+			dnd_doc = frappe.get_doc("Delivery Note Item", dnd[0])
+			cetsh_number = frappe.get_value("Item", dnd[1], "customs_tariff_number")
+			if cetsh_number:
+				frappe.db.set_value("Delivery Note Item", dnd[0], "cetsh_number", cetsh_number)
+				frappe.db.set_value("Delivery Note Item", dnd[0], "gst_hsn_code", cetsh_number)
+				frappe.db.commit()
+				print("Updated CETSH Number and GST HSN Code in Delivery Note # " \
+					+ dnd_doc.parent + " Item No: " + str(dnd_doc.idx))
+			else:
+				print("DN# " + dnd_doc.parent + " Item Code: " + dnd[1] + \
+					" At Row No " + str(dnd_doc.idx) + \
+					" Does Not Have CETSH Number Linked")
+
+
+	dnd_list = frappe.db.sql("""SELECT name, gst_hsn_code, cetsh_number FROM `tabDelivery Note Item` 
+		WHERE cetsh_number IS NOT NULL AND gst_hsn_code IS NULL 
+		ORDER BY creation""", as_list=1)
+	if dnd_list:
+		for dnd in dnd_list:
+			cetsh_number = frappe.get_value("Delivery Note Item", dnd[0], "cetsh_number")
+			dnd_doc = frappe.get_doc("Delivery Note Item", dnd[0])
+			frappe.db.set_value("Delivery Note Item", dnd[0], "gst_hsn_code", cetsh_number)
+			frappe.db.commit()
+			print("Updated GST HSN Code in DN # " + dnd_doc.parent + " Item No: " + str(dnd_doc.idx))
+
+	sid_no_cetsh = frappe.db.sql("""SELECT name, item_code ,gst_hsn_code, cetsh_number FROM `tabSales Invoice Item` 
+		WHERE cetsh_number IS NULL AND gst_hsn_code IS NULL 
+		ORDER BY creation""", as_list=1)
+	if sid_no_cetsh:
+		for sid in sid_no_cetsh:
+			sid_doc = frappe.get_doc("Sales Invoice Item", sid[0])
+			cetsh_number = frappe.get_value("Item", sid[1], "customs_tariff_number")
+			if cetsh_number:
+				frappe.db.set_value("Sales Invoice Item", dnd[0], "cetsh_number", cetsh_number)
+				frappe.db.set_value("Sales Invoice Item", dnd[0], "gst_hsn_code", cetsh_number)
+				frappe.db.commit()
+				print("Updated CETSH Number and GST HSN Code in Sales Invoice # " \
+					+ sid_doc.parent + " Item No: " + str(sid_doc.idx))
+			else:
+				print("SI# " + sid_doc.parent + " Item Code: " + sid[1] + \
+					" At Row No " + str(sid_doc.idx) + \
+					" Does Not Have CETSH Number Linked")
+
+	sid_list = frappe.db.sql("""SELECT name, gst_hsn_code, cetsh_number FROM `tabSales Invoice Item` 
+		WHERE cetsh_number IS NOT NULL AND gst_hsn_code IS NULL 
+		ORDER BY creation""", as_list=1)
+	if sid_list:
+		for sid in sid_list:
+			cetsh_number = frappe.get_value("Sales Invoice Item", sid[0], "cetsh_number")
+			sid_doc = frappe.get_doc("Sales Invoice Item", sid[0])
+			frappe.db.set_value("Sales Invoice Item", sid[0], "gst_hsn_code", cetsh_number)
+			frappe.db.commit()
+			print("Updated GST HSN Code in SI # " + sid_doc.parent + " Item No: " + str(sid_doc.idx))
