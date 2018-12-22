e10c730ac8f7ad27ec70df42cf453607f9cac4bb
Aditya Duggal <adityaduggal@gmail.com>
2018-12-22 06:53:34 +0530

    Added Quotation to the Patch

--- rohit_common/patches/20181222_update_gst_hsn.py
+++ rohit_common/patches/20181222_update_gst_hsn.py
@@ -3,6 +3,35 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
+	qod_no_cetsh = frappe.db.sql("""SELECT name, item_code ,gst_hsn_code, cetsh_number FROM `tabQuotation Item` 
+		WHERE cetsh_number IS NULL AND gst_hsn_code IS NULL 
+		ORDER BY creation""", as_list=1)
+	if qod_no_cetsh:
+		for qod in sod_no_cetsh:
+			qod_doc = frappe.get_doc("Quotation Item", qod[0])
+			cetsh_number = frappe.get_value("Item", qod[1], "customs_tariff_number")
+			if cetsh_number:
+				frappe.db.set_value("Quotation Item", qod[0], "cetsh_number", cetsh_number)
+				frappe.db.set_value("Quotation Item", qod[0], "gst_hsn_code", cetsh_number)
+				frappe.db.commit()
+				print("Updated CETSH Number and GST HSN Code in Quotation # " \
+					+ qod_doc.parent + " Item No: " + str(qod_doc.idx))
+			else:
+				print("QTN# " + qod_doc.parent + " Item Code: " + qod[1] + \
+					" At Row No " + str(qod_doc.idx) + \
+					" Does Not Have CETSH Number Linked")
+
+	qod_list = frappe.db.sql("""SELECT name, gst_hsn_code, cetsh_number FROM `tabQuotation Item` 
+		WHERE cetsh_number IS NOT NULL AND gst_hsn_code IS NULL 
+		ORDER BY creation""", as_list=1)
+	if qod_list:
+		for qod in qod_list:
+			cetsh_number = frappe.get_value("Quotation Item", qod[0], "cetsh_number")
+			qod_doc = frappe.get_doc("Quotation Item", qod[0])
+			frappe.db.set_value("Quotation Item", qod[0], "gst_hsn_code", cetsh_number)
+			frappe.db.commit()
+			print("Updated GST HSN Code in Quotation # " + qod_doc.parent + " Item No: " + str(qod_doc.idx))
+
 	sod_no_cetsh = frappe.db.sql("""SELECT name, item_code ,gst_hsn_code, cetsh_number FROM `tabSales Order Item` 
 		WHERE cetsh_number IS NULL AND gst_hsn_code IS NULL 
 		ORDER BY creation""", as_list=1)
