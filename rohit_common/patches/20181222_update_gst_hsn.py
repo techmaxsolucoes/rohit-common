439786f09d4fc47fb7e411dc79e3c2e65e115e54
Aditya Duggal <adityaduggal@gmail.com>
2018-12-22 07:55:55 +0530

    Minor FIx

--- rohit_common/patches/20181222_update_gst_hsn.py
+++ rohit_common/patches/20181222_update_gst_hsn.py
@@ -11,8 +11,8 @@ def execute():
 			sid_doc = frappe.get_doc("Sales Invoice Item", sid[0])
 			cetsh_number = frappe.get_value("Item", sid[1], "customs_tariff_number")
 			if cetsh_number:
-				frappe.db.set_value("Sales Invoice Item", dnd[0], "cetsh_number", cetsh_number)
-				frappe.db.set_value("Sales Invoice Item", dnd[0], "gst_hsn_code", cetsh_number)
+				frappe.db.set_value("Sales Invoice Item", sid[0], "cetsh_number", cetsh_number)
+				frappe.db.set_value("Sales Invoice Item", sid[0], "gst_hsn_code", cetsh_number)
 				print("Updated CETSH Number and GST HSN Code in Sales Invoice # " \
 					+ sid_doc.parent + " Item No: " + str(sid_doc.idx))
 			else:
