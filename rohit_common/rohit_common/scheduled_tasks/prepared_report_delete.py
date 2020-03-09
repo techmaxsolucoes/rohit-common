4ae3af958780eefefbbfec68f0c2062fabe482f1
Aditya Duggal <adityaduggal@gmail.com>
2020-03-09 14:15:53 +0530

    Minor Fix

--- rohit_common/rohit_common/scheduled_tasks/prepared_report_delete.py
+++ rohit_common/rohit_common/scheduled_tasks/prepared_report_delete.py
@@ -6,7 +6,7 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.utils.file_manager import delete_file_from_filesystem
+from frappe.utils.file_manager import delete_file
 
 def execute():
 	set_days = 300
@@ -20,19 +20,15 @@ def execute():
 		print(str(sno) + " Deleting Prepared Report# " + pr[0])
 		#frappe.db.sql("""DELETE FROM `tabPrepared Report` WHERE name = '%s'"""%(pr[0]))
 		print ("Deleting File Data for Prepared Report# " + pr[0])
-		file_name = frappe.db.sql("""SELECT name, file_name, is_private FROM `tabFile` 
+		file_name = frappe.db.sql("""SELECT name, file_name, is_private, file_url FROM `tabFile` 
 			WHERE attached_to_doctype = 'Prepared Report' 
 			AND attached_to_name = '%s' """%(pr[0]), as_list=1)
 
 		frappe.db.sql("""DELETE FROM `tabFile` WHERE attached_to_doctype = 'Prepared Report' 
 			AND attached_to_name = '%s'"""%(pr[0]))
 		print ("Deleting the Actual File from System for Prepared Report# " + pr[0])
-		print (file_name[0])
-		file_doc = frappe.get_doc('File', file_name[0])
-		delete_file_from_filesystem(file_doc)
-
-
-
+		delete_file(file_name[3])
+		
 		#frappe.delete_doc('Prepared Report', pr[0])
 		if sno%50==0:
 			print ("Committing Changes Current Files Done = " + str(sno))
