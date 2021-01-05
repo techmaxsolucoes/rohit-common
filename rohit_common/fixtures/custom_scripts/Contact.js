221e08f3fdb7c305d903b30f55c3d52b948eea10
Aditya Duggal <adityaduggal@gmail.com>
2021-01-05 12:44:38 +0530

    Added a custom Script on Address to remove the Email Validation

---
+++ rohit_common/fixtures/custom_scripts/Contact.js
@@ -0,0 +1,5 @@
+frappe.ui.form.on("Contact Email", "email_id", function(frm, cdt, cdn){
+	var d = locals[cdt][cdn]
+	frappe.model.set_value(cdt, cdn, "validated", 0);
+	cur_frm.refresh_fields();
+});
