b0dcb66507e2a859c82e84cad0e830c1c01327cd
Aditya Duggal <adityaduggal@gmail.com>
2021-01-07 23:14:47 +0530

    fix: Added settings for File Attachments

--- rohit_common/rohit_common/doctype/rohit_settings/rohit_settings.py
+++ rohit_common/rohit_common/doctype/rohit_settings/rohit_settings.py
@@ -6,5 +6,21 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
+
 class RohitSettings(Document):
-	pass
+    def validate(self):
+        self.sort_single_field_child("roles_allow_pub_att", "role")
+        self.sort_single_field_child("docs_with_pub_att", "document_type")
+        self.sort_single_field_child("auto_convert_pub_priv", "document_type")
+
+    def sort_single_field_child(self, table_name, field_name):
+        sorted_table = []
+        row_dict = {}
+        for row in self.get(table_name):
+            row_dict[field_name] = row.get(field_name)
+
+            sorted_table.append(row_dict.copy())
+        sorted_table = sorted(sorted_table, key=lambda i: i[field_name], reverse=0)
+        self.set(table_name, [])
+        for d in sorted_table:
+            self.append(table_name, d)
