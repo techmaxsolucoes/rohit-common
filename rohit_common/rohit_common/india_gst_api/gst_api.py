b86a74a09dee66d382b01eb4df16fccf1d16d436
Aditya Duggal <adityaduggal@gmail.com>
2022-02-27 17:59:46 +0530

    fix: Minor Fix to the code as per pylinter

--- rohit_common/rohit_common/india_gst_api/gst_api.py
+++ rohit_common/rohit_common/india_gst_api/gst_api.py
@@ -1,99 +1,114 @@
-#  Copyright (c) 2021. Rohit Industries Group Private Limited and Contributors.
+#  Copyright (c) 2022. Rohit Industries Group Private Limited and Contributors.
 #  For license information, please see license.txt
 # -*- coding: utf-8 -*-
-import frappe
 import datetime
+import frappe
 import requests
-from frappe.utils import flt, getdate
+from frappe.utils import flt
 from .common import get_gsp_details, get_api_version
-timeout = 7
+TIMEOUT = 7
 
 
 def get_gstr1(gstin, ret_period, action):
+    """
+    Returns JSON for GSTR1 for a given GSTIN, Return Period and action is Required
+    Action is basically Sub-type like B2B etc for Returns
+    """
     api = "gstr1"
     auth_token = get_auth_token(gstin)
     url = get_gst_url(api, action, gstin) + "&authtoken=" + auth_token + "&ret_period=" + ret_period
     resp = get_gst_response(url=url, type_of_req="get")
-    # frappe.throw(str(resp))
     if flt(resp.get("status_cd")) == 1:
-        return resp
+        pass
     elif resp.get(action.lower(), None) is not None:
-        return resp
+        pass
     elif flt(resp.get("status_cd")) == 0:
-        return []
+        resp = []
     else:
         frappe.throw(f"Some Error in Response with Error Message = {resp} URL Used= {url}")
+    return resp
 
 
 def get_gstr2a(gstin, ret_period, action):
+    """
+    Returs JSON for GSTR2A for a GSTIN, Return Period and Action is also required
+    Action is basically Sub-type like B2B etc for Returns
+    """
     api = "gstr2a"
     auth_token = get_auth_token(gstin)
     url = get_gst_url(api, action, gstin) + "&authtoken=" + auth_token + "&ret_period=" + ret_period
     resp = get_gst_response(url=url, type_of_req="get")
     if flt(resp.get("status_cd")) == 1:
-        return resp
+        pass
     elif resp.get(action.lower(), None) is not None:
-        return resp
+        pass
     elif flt(resp.get("status_cd")) == 0:
-        return []
+        resp = []
     else:
         frappe.throw(f"Some Error in Response with Error Message = {resp} URL Used= {url}")
+    return resp
 
 
 def get_gstr2b(gstin, ret_period, action, file_num=None):
+    """
+    Returns JSON for GSTR2B for GSTIN, Return Period and Action is Required.
+    Action is basically Sub-type like B2B etc for Returns
+    """
     if file_num:
         file_num_link = "&file_num" + file_num
     else:
         file_num_link = ""
     api = "gstr2b"
     auth_token = get_auth_token(gstin)
-    url = get_gst_url(api, action, gstin) + "&authtoken=" + auth_token + "&rtnprd=" + ret_period + "&ret_period=" + \
-          ret_period + file_num_link
-    # frappe.throw(url)
+    url = get_gst_url(api, action, gstin) + "&authtoken=" + auth_token + "&rtnprd=" + ret_period + \
+        "&ret_period=" + ret_period + file_num_link
     resp = get_gst_response(url=url, type_of_req="get")
-    # frappe.msgprint(f"{resp}")
     if resp.get("chksum"):
-        return resp
+        pass
     else:
         frappe.throw(f"Some Error in Response with Error Message = {resp} URL Used= {url}")
+    return resp
 
 
 def get_gst_response(url, type_of_req):
+    """
+    Based on Type of Request it would do a post or get request for a URL given
+    """
     if type_of_req == "post":
-        return requests.post(url=url, timeout=timeout).json()
+        response = requests.post(url=url, timeout=TIMEOUT).json()
     elif type_of_req == "get":
-        return requests.get(url=url, timeout=timeout).json()
+        response = requests.get(url=url, timeout=TIMEOUT).json()
     else:
         frappe.throw(f"Type of Request {type_of_req} is Not Defined")
+    return response
 
 
 @frappe.whitelist()
 def get_gst_otp(gstin):
+    """
+    Send a request to GST server to send OTP to registered mobile number of a given GST
+    """
     api = "otp"
     action = "OTPREQUEST"
     otp_url = get_gst_url(api=api, action=action, gstin=gstin)
-    res = requests.get(otp_url, timeout=timeout).json()
+    res = requests.get(otp_url, timeout=TIMEOUT).json()
     if flt(res.get("status_cd")) == 1:
         frappe.msgprint("OTP Sent Successfully, Please enter the OTP sent to RMN")
     else:
-        frappe.msgprint(f"OTP Generation Failed. Please try again later. Error Message is {res} and OTP URL {otp_url}")
+        frappe.msgprint(f"OTP Generation Failed. Please try again later. Error Message is {res} \
+            and OTP URL {otp_url}")
 
 
 @frappe.whitelist()
 def authenticate_gst_otp(gstin, otp, row_id):
-    # gst_reg = frappe.get_doc("GST Registration Details", row_id)
+    """
+    To authenticate the OTP received on RMN this is used
+    """
     api = "otp"
     action = "AUTHTOKEN"
     auth_url = get_gst_url(api=api, action=action, gstin=gstin) + "&OTP=" + otp
-    #print(auth_url)
-    # auth_url = "http://gstsandbox.charteredinfo.com/taxpayerapi/dec/v1.0/authenticate?
-    # action=AUTHTOKEN&aspid=1641491220&password=NRjnagyWb36@b5kC&gstin=27GSPMH0041G1ZZ
-    # &username=Chartered.MH.1&OTP=575757"
-    # print(auth_url)
-    auth_resp = requests.get(url=auth_url, timeout=timeout)
-    print(auth_resp)
+    auth_resp = requests.get(url=auth_url, timeout=TIMEOUT)
     auth_resp = auth_resp.json()
-    print(auth_resp)
     if flt(auth_resp.get("status_cd")) == 1:
         frappe.db.set_value("GST Registration Details", row_id, "last_otp_sent",
                 datetime.datetime.now())
@@ -105,12 +120,17 @@ def authenticate_gst_otp(gstin, otp, row_id):
 
 
 def check_for_refresh_token(row):
+    """
+    For a row dict would check if the Auth Token is needed to be refreshed for a GSTIN
+    """
     now_time = datetime.datetime.now()
     token_time = flt(row.update_token_every_mins)
     if row.authorization_token and row.api_access_authorized == 1:
-        token_update_time = row.validity_of_token - datetime.timedelta(minutes=360) + datetime.timedelta(minutes=token_time)
-        if token_update_time < now_time and row.validity_of_token > now_time:
-            new_auth_token, error_code = refresh_auth_token(row.gst_registration_number, row.authorization_token)
+        token_update_time = row.validity_of_token - datetime.timedelta(minutes=360) + \
+            datetime.timedelta(minutes=token_time)
+        if token_update_time < now_time < row.validity_of_token:
+            new_auth_token, error_code = refresh_auth_token(row.gst_registration_number,
+                row.authorization_token)
             if new_auth_token != "":
                 print(f"Auto Updated Auth Token for {row.gst_registration_number}")
                 update_auth_token(row_name=row.name, auth_token=new_auth_token)
@@ -118,9 +138,11 @@ def check_for_refresh_token(row):
                 print(f"Auto Auth Token Update Failed for {row.gst_registration_number}")
                 update_auth_token(row_name=row.name, auth_token="", failed=1, error_code=error_code)
         elif token_update_time > now_time and row.validity_of_token > now_time:
-            print(f"No Need to Update the Auth Token for {row.gst_registration_number} as Token to be Updated After {token_update_time}")
+            print(f"No Need to Update the Auth Token for {row.gst_registration_number} as Token to \
+                be Updated After {token_update_time}")
         else:
-            print(f"Auth Token is Expired and hence Need to Generate OTP Again for {row.gst_registration_number}")
+            print(f"Auth Token is Expired and hence Need to Generate OTP Again for \
+                {row.gst_registration_number}")
             update_auth_token(row_name=row.name, auth_token="", failed=1, error_code='AUTH4037')
     elif row.api_access_authorized != 1:
         print(f"API Authorization is Unchecked for {row.gst_registration_number}")
@@ -129,51 +151,70 @@ def check_for_refresh_token(row):
 
 
 def refresh_auth_token(gstin, auth_token):
+    """
+    Tries to refresh the auth token for a given GSTIN and Auth Token
+    """
     error_code = ""
     api = "otp"
     action = "REFRESHTOKEN"
     ref_url = get_gst_url(api=api, action=action, gstin=gstin) + "&AuthToken=" + auth_token
-    ref_resp = requests.get(url=ref_url, timeout=timeout).json()
+    ref_resp = requests.get(url=ref_url, timeout=TIMEOUT).json()
     if flt(ref_resp.get("status_cd")) == 1:
-        return ref_resp.get("auth_token"), error_code
+        refresh_resp = ref_resp.get("auth_token")
     else:
+        refresh_resp = ""
         error = ref_resp.get('error')
-        print(f"Some Error in Refreshing the Auth Token with Error = {ref_resp.get('error')} and URL "
-                        f"being used = {ref_url} and full response is {ref_resp}")
+        print(f"Some Error in Refreshing the Auth Token with Error = {ref_resp.get('error')} and \
+            URL being used = {ref_url} and full response is {ref_resp}")
         error_code = error.get("error_cd")
-        return "", error_code
+    return refresh_resp, error_code
 
 
 def get_auth_token(gstin):
+    """
+    Gets auth token for a given GSTIN
+    """
     found = 0
     r_set = frappe.get_doc("Rohit GST Settings", "Rohit GST Settings")
     now_time = datetime.datetime.now()
-    for d in r_set.gst_registration_details:
-        if d.gst_registration_number == gstin:
-            token_update_min = flt(d.update_token_every_mins)  # Updates token after this many mins
-            token_update_time = d.validity_of_token - datetime.timedelta(minutes=360) + datetime.timedelta(minutes=token_update_min)
+    new_auth_token = ""
+    for row in r_set.gst_registration_details:
+        if row.gst_registration_number == gstin:
+            # Updates token after this many mins
+            token_update_min = flt(row.update_token_every_mins)
+            token_update_time = row.validity_of_token - datetime.timedelta(minutes=360) + \
+                datetime.timedelta(minutes=token_update_min)
             found = 1
-            if d.authorization_token and d.api_access_authorized == 1:
-                if token_update_time > now_time and d.validity_of_token > now_time:
-                    return d.authorization_token
-                elif token_update_time < now_time < d.validity_of_token:
-                    new_auth_token, error_code = refresh_auth_token(gstin=gstin, auth_token=d.authorization_token)
+            if row.authorization_token and row.api_access_authorized == 1:
+                if token_update_time > now_time and row.validity_of_token > now_time:
+                    new_auth_token = row.authorization_token
+                elif token_update_time < now_time < row.validity_of_token:
+                    new_auth_token, error_code = refresh_auth_token(gstin=gstin,
+                        auth_token=row.authorization_token)
                     if new_auth_token != "":
-                        update_auth_token(row_name=d.name, auth_token=new_auth_token, error_code=error_code)
-                        return new_auth_token
+                        update_auth_token(row_name=row.name, auth_token=new_auth_token,
+                            error_code=error_code)
                     else:
-                        update_auth_token(row_name=d.name, auth_token="", failed=1, error_code=error_code)
-                        frappe.throw(f"New Auth Token for {gstin} is Empty you might need to Generate OTP Again")
+                        update_auth_token(row_name=row.name, auth_token="", failed=1,
+                            error_code=error_code)
+                        frappe.throw(f"New Auth Token for {gstin} is Empty you might need to \
+                            Generate OTP Again")
                 else:
-                    update_auth_token(row_name=d.name, auth_token="", failed=1, error_code="AUTH4037")
-                    frappe.throw(f"Auth Token for {gstin} is Expired you might need to Generate OTP Again")
+                    update_auth_token(row_name=row.name, auth_token="", failed=1,
+                        error_code="AUTH4037")
+                    frappe.throw(f"Auth Token for {gstin} is Expired you might need to \
+                        Generate OTP Again")
             else:
                 frappe.throw(f"Authorization Needed for {gstin}. Resend OTP and Get Authorization")
     if found == 0:
         frappe.throw(f"{gstin} is Not Setup in Rohit GST Settings for API Access")
+    return new_auth_token
 
 
 def update_auth_token(row_name, auth_token, failed=0, error_code=None):
+    """
+    Updates the Auth Token for a Row Name in the GST Registration Details
+    """
     if not error_code:
         error_code = ""
     ecl = ["AUTH4037", "RET11402", "SWEB9033", "AUTH4033", "GSP102"]
@@ -181,67 +222,82 @@ def update_auth_token(row_name, auth_token, failed=0, error_code=None):
         # Only Update API Access to zero on certain errors and not all errors
         frappe.db.set_value("GST Registration Details", row_name, "otp", "")
         if error_code != "" and error_code in ecl:
-            print(f"Removing API Access")
+            print("Removing API Access")
             frappe.db.set_value("GST Registration Details", row_name, "api_access_authorized", 0)
         else:
             print(f"Error Code: {str(error_code)} Not in List")
         frappe.db.commit()
     else:
-        auth_access = flt(frappe.db.get_value("GST Registration Details", row_name, "api_access_authorized"))
-        exist_token_times = flt(frappe.db.get_value("GST Registration Details", row_name, "no_of_times_token_updated"))
+        auth_access = flt(frappe.db.get_value("GST Registration Details", row_name,
+            "api_access_authorized"))
+        exist_token_times = flt(frappe.db.get_value("GST Registration Details", row_name,
+            "no_of_times_token_updated"))
         if auth_access == 1:
-            frappe.db.set_value("GST Registration Details", row_name, "no_of_times_token_updated", exist_token_times + 1)
+            frappe.db.set_value("GST Registration Details", row_name, "no_of_times_token_updated",
+                exist_token_times + 1)
         else:
-            frappe.db.set_value("GST Registration Details", row_name, "no_of_times_token_updated", 0)
+            frappe.db.set_value("GST Registration Details", row_name, "no_of_times_token_updated",
+                0)
         frappe.db.set_value("GST Registration Details", row_name, "api_access_authorized", 1)
         frappe.db.set_value("GST Registration Details", row_name, "authorization_token", auth_token)
         frappe.db.set_value("GST Registration Details", row_name, "otp", "")
         frappe.db.set_value("GST Registration Details", row_name, "validity_of_token",
                             datetime.datetime.now() + datetime.timedelta(minutes=360))
 
-def logout_gst():
-    url = "http://gstsandbox.charteredinfo.com/taxpayerapi/dec/v1.0/authenticate?action=LOGOUT&aspid=1641491220&password=NRjnagyWb36@b5kC&gstin=27GSPMH0041G1ZZ&username=Chartered.MH.1&authtoken=295898a7acb348f29f67afbada72e4b4"
-    resp = requests.get(url)
-    print(resp)
-    resp = resp.json()
-    print(resp)
-
 
 def get_gst_url(api, action, gstin):
+    """
+    Gets the URL for a GSTIN api, action and GSTIN
+    """
     gsp_link, asp_id, asp_pass, gstin_other, sandbox = get_gsp_details(api=api, action=action)
     username = get_gst_username(gstin=gstin)
     version, act_type = get_api_version(api)
-    gst_url = gsp_link + "/taxpayerapi/dec/" + version + "/" + act_type + "?action=" + action + "&aspid=" + asp_id + \
-              "&password=" + asp_pass + "&gstin=" + gstin + "&username=" + username
+    gst_url = gsp_link + "/taxpayerapi/dec/" + version + "/" + act_type + "?action=" + action + \
+        "&aspid=" + asp_id + "&password=" + asp_pass + "&gstin=" + gstin + "&username=" + username
     return gst_url
 
 
 def get_gst_username(gstin):
+    """
+    Get the GST Username from Rohit GST Settings for a given GSTIN
+    """
     found = 0
+    gst_username = ""
     r_set = frappe.get_doc("Rohit GST Settings", "Rohit GST Settings")
-    for d in r_set.gst_registration_details:
-        if d.gst_registration_number == gstin:
+    for row in r_set.gst_registration_details:
+        if row.gst_registration_number == gstin:
             found = 1
-            return d.gst_username
+            gst_username = row.gst_username
     if found == 0:
         frappe.throw(f"No GSTIN username found for {gstin}")
+    return gst_username
 
 
 def get_invoice_type(itype):
+    """
+    Returns the Invoice Type for a Invoice Type code
+    """
+    inv_type_txt = ""
     if itype == "R":
-        return "R-Regular B2B Invoices"
+        inv_type_txt = "R-Regular B2B Invoices"
     elif itype == "DE":
-        return "DE-Deemed Exports"
+        inv_type_txt = "DE-Deemed Exports"
     elif itype == "SEWP":
-        return "SEWP-SEZ Exports with payment"
+        inv_type_txt = "SEWP-SEZ Exports with payment"
     elif itype == "SEWOP":
-        return "SEWOP-SEZ exports without payment"
+        inv_type_txt = "SEWOP-SEZ exports without payment"
+    return inv_type_txt
 
 
 def get_type_of_amend(amend_type):
+    """
+    Returns text for a the Type of Amend to the Invoice for a Code
+    """
+    amd_type_txt = ""
     if amend_type == "R":
-        return "R-Receiver GSTIN Amended"
+        amd_type_txt = "R-Receiver GSTIN Amended"
     elif amend_type == "N":
-        return "N-Invoice Number Amended"
+        amd_type_txt = "N-Invoice Number Amended"
     elif amend_type == "D":
-        return "D-Details Amended"
+        amd_type_txt = "D-Details Amended"
+    return amd_type_txt
