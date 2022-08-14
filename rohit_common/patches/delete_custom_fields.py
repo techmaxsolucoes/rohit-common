e13b3233ac37aae0eea001cdca4ec3a4755015c0
Aditya Duggal <adityaduggal@gmail.com>
2022-08-15 01:57:54 +0530

    fix: Removed Billing Address field from Doctypes 	- Purchase Invoice 	- Purchase Order reason: Duplicate field as its now a standard field in erpnext

--- rohit_common/patches/delete_custom_fields.py
+++ rohit_common/patches/delete_custom_fields.py
@@ -2,11 +2,15 @@
 from __future__ import unicode_literals
 import frappe
 
+
 def execute():
     # Deletes custom fields from Custom Field Table based on Dictionary
-    custom_field_list = [{"dt": "Asset", "fieldname": "autoname"}
+    custom_field_list = [{"dt": "Asset", "fieldname": "autoname"},
+                         {"dt": "Purchase Order", "fieldname": "billing_address"},
+                         {"dt": "Purchase Invoice", "fieldname": "billing_address"}
                          ]
     for fld in custom_field_list:
         custom_field = frappe.db.get_value("Custom Field", fld)
-        frappe.delete_doc("Custom Field", custom_field)
-        print(f"Deleted Custom Field: {custom_field} with Filters: {fld}")
+        if custom_field:
+            frappe.delete_doc("Custom Field", custom_field)
+            print(f"Deleted Custom Field: {custom_field} with Filters: {fld}")
