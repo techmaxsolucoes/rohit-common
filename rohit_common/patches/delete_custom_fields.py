3be952ca7fae82ccef4f3f019e54ee2a92f5fb5e
Aditya Duggal <adityaduggal@gmail.com>
2023-09-26 12:40:14 +0530

    fix: Removed custom field from Purchase Order Item as now available in standard erpnext

--- rohit_common/patches/delete_custom_fields.py
+++ rohit_common/patches/delete_custom_fields.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 import frappe
 
 
 def execute():
     # Deletes custom fields from Custom Field Table based on Dictionary
-    custom_field_list = [{"dt": "Asset", "fieldname": "autoname"},
-                         {"dt": "Purchase Order", "fieldname": "billing_address"},
-                         {"dt": "Purchase Invoice", "fieldname": "billing_address"}
-                         ]
+    custom_field_list = [
+        {"dt": "Asset", "fieldname": "autoname"},
+        {"dt": "Purchase Order", "fieldname": "billing_address"},
+        {"dt": "Purchase Invoice", "fieldname": "billing_address"},
+        {"dt": "Purchase Order Item", "fieldname": "from_warehouse"},
+    ]
     for fld in custom_field_list:
         custom_field = frappe.db.get_value("Custom Field", fld)
         if custom_field:
