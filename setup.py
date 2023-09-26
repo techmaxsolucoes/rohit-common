79566a0958e8ec69ea0c33a29224927cdec6dd58
Aditya Duggal <adityaduggal@gmail.com>
2023-09-26 12:40:50 +0530

    fix: Added PyProject toml and removed values from setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# TODO: Remove this file when  v15.0.0 is released
+from setuptools import setup
 
-import ast
-import re
+name = "rohit_common"
 
-from setuptools import find_packages, setup
-
-# get version from __version__ variable in rohit_common/__init__.py
-_version_re = re.compile(r"__version__\s+=\s+(.*)")
-
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
-with open("rohit_common/__init__.py", "rb") as f:
-    version = str(ast.literal_eval(
-        _version_re.search(f.read().decode("utf-8")).group(1)))
-
-setup(
-    name='rohit_common',
-    version=version,
-    description='Rohit ERPNext Extensions (Common)',
-    author='Rohit Industries Group Pvt Ltd.',
-    author_email='aditya@rigpl.com',
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=install_requires,
-)
+setup()
